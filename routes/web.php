199acd78d87b89b42f0e7d20125e5bc6daf60e95
unknown <MP@HP50634.mp.pa.gov.br>
2022-12-14 08:12:00 -0300

    listando episódios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use App\Models\Episode;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +26,8 @@ Route::resource('/series', SeriesController::class)
     ->except(['show']);
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('season/{season}/episodes', [EpisodesController::class, 'index'])-> name('episodes.index');
+Route::post('season/{season}/episodes', function(Request $request){
+    dd($request->all());
+});
